3790b82070caeda26827f0026b0caf666cc2c319
Hannah <hannahwoodward@hotmail.com>
2021-10-23 09:04:08 +0800

    not complete yet

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your code here
+
+fetch('http://localhost:3000/users' , {
+    method: 'POST',
+    headers: {
+        "Content-type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        'name':"Hannah",
+        'email': "hannah@gmail.com"
+    })
+})
